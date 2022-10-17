3618c0eb1cd428196b74f4c63102fe685f5d105b
Vanessa Vasquez <vvasquez528@gmail.com>
2022-10-17 17:44:09 -0400

    Set up starter code

---
+++ src/main/java/com/example/questionnaireapi/QuestionnaireApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionnaireApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionnaireApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/questionnaireapi/QuestionnaireApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.questionnaireapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionnaireApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
