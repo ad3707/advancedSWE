15942a7bf53dc6c30928e9df0e26ca93c5ec113c
Aditi Dam <dama2468@gmail.com>
2022-10-22 12:27:51 -0400

    Added unit testing for updating questions,creating questions, and deleteing questions

--- src/test/java/com/example/sweProject/QuestionControllerTest.java
+++ src/test/java/com/example/sweProject/QuestionControllerTest.java
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import com.example.sweProject.controllers.QuestionController;
 import com.example.sweProject.entities.Question;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -15,6 +18,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.willReturn;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+
 import static org.hamcrest.core.Is.is;
 
 import java.util.*;
@@ -51,4 +56,51 @@ public class QuestionControllerTest {
                .andExpect(jsonPath("name", is(q.getName())))
                .andExpect(jsonPath("A", is(q.getA())));
    }
+
+   //Test for the POST json request. Tests if a user can add a question
+   @Test
+   public void createQuestionAPI() throws Exception {
+      mvc.perform( MockMvcRequestBuilders
+               .post("/questions")
+               .content(asJsonString(new newQuestion("question", "choiceA", "choiceB", "choiceC", "choiceD","answer")))
+               .contentType(MediaType.APPLICATION_JSON))
+               .andExpect(status().isCreated())
+               .andExpect(MockMvcResultMatchers.jsonPath("$.questionID").exists());
+   }
+
+   public static String asJsonString(final Object obj) {
+      try {
+          return new ObjectMapper().writeValueAsString(obj);
+      } catch (Exception e) {
+          throw new RuntimeException(e);
+      }
+  }
+
+
+//Test for the PUT API. Tests if client can update a question
+  @Test
+  public void updateQuestioAPI() throws Exception {
+   mvc.perform( MockMvcRequestBuilders
+               .put("/update/{id}",3)
+               .content(asJsonString(new newQuestion(3,"1 + 2","3","4","5","6","3")))
+               .contentType(MediaType.APPLICATION_JSON)
+               .accept(MediaType.APPLICATION_JSON))
+               .andExpect(status().isOk())
+               .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("1 + 2"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.a").value("3"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.b").value("4"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.c").value("5"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.d").value("6"))
+               .andExpect(MockMvcResultMatchers.jsonPath("$.answer").value("3"));
+  }
+
+
+
+//Test for the DELETE API. Tests if a client can delete a quesiton.
+@Test
+public void deleteQuestionAPI() throws Exception {
+   mvc.perform( MockMvcRequestBuilders.delete("/delete/{id}",3))
+               .andExpect(status().isAccepted());
+   }
+  
 }
